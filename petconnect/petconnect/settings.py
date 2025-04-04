7b3c75d1cfa6608689639a3a6b4c573587d6ed9d
Sneha1262 <sne6124@gmail.com>
2025-04-05 00:33:05 +0100

    ch

--- petconnect/petconnect/settings.py
+++ petconnect/petconnect/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-wm7eap#$38a5(gf#er3_&rhzjpiaf!*x#tnea*+zszd_y&63t!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['13.216.172.195', 'localhost', '127.0.0.1']
+#ALLOWED_HOSTS = ['13.216.172.195', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS = [
