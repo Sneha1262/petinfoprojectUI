1ea606da76154f85fb3cf5820f875e0b9033c710
Sneha1262 <sne6124@gmail.com>
2025-04-05 00:09:47 +0100

    change

--- petconnect/petconnect/settings.py
+++ petconnect/petconnect/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wm7eap#$38a5(gf#er3_&rhzjpiaf!*x#tnea*+zszd_y&63t!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
